842cfb259e69f4bf94dc1afb3b9970f0ae61d939
Ena Markovic <ena.markovic@smartsheet.com>
2019-05-10 13:14:26 -0700

    Added second request

--- adobe-auth.js
+++ adobe-auth.js
@@ -1,51 +1,79 @@
 //part 1 folllow this : https://secure.na1.echosign.com/public/static/oauthDoc.jsp
 
 
-var redirect_uri = "https://example.com/oauthDemo";
+var redirect_uri = "https://emarkovic.github.io/adobe-auth/";
 var response_type = "code";
-var scope = "agreement_read:account+agreement_send:account+agreement_write:account+library_read:account+library_write:account+user_login:account+user_read:account+user_write:account+webhook_read:account+webhook_retention:account+webhook_write:account+widget_read:account+widget_write:account+workflow_read:account+workflow_write";
+var scope = "user_login:self+agreement_write:account+agreement_send:account:widget_write:account";
 
 var grant_type = "authorization_code"
 
 var submitBtn = document.getElementById("submitBtn");
 submitBtn.addEventListener("click", () => {
-	var client_id = document.getElementById("clientId").value;
-	if (client_id && client_id.trim().length) {
-		localStorage.setItem('client_id', client_id);
-		submitBtn.value = "";
+	var clientIdField = document.getElementById("clientId");
+	var clientSecretField = document.getElementById("client_secret");
+
+	var client_id = clientIdField.value;
+	var client_secret = clientSecretField.value;
+
+	if (paramExists(client_id) && paramExists(client_secret)) {
+		localStorage.setItem("client_id", client_id);
+		localStorage.setItem("client_secret", client_secret);
+
+		clientIdField.value = "";
+		clientSecretField.value = "";
+
 		getAuthorizationCode();
 	}
 });
+
+window.onload = () => {
+	var currentUrl = new URL(window.location.href);
+	var authorization_code = currentUrl.searchParams.get("code");
+	if (paramExists(authorization_code)) {
+		localStorage.setItem("authorization_code", authorization_code);
+		getAccessToken();
+	}
+}
 
 
 function getAuthorizationCode() {
 	var url = "https://secure.na1.echosign.com/public/oauth";
 	var client_id = localStorage.getItem("client_id");
 	
-	console.log(client_id);
-	if (client_id && client_id.trim().length) {
+	if (paramExists(client_id)) {
 		var urlParams = "?redirect_uri=" + redirect_uri + 
 				"&response_type=" + response_type + 
 				"&client_id=" + client_id +
-				"&scope=user_login:self+agreement_write:account+agreement_send:account:widget_write:account";
+				"&scope=" + scope;
 
 		document.getElementById("signIn").href = url + urlParams;
-		
 	}
 }
 
 
 
 function getAccessToken() {
-
 	var url = "https://api.na2.echosign.com/oauth/token";
-	var urlParams = "?grant_type=authorization_code&client_id=CBJCHBCAABAAC-U7Qyp9MHLMtUI2VFCdHSreCjnPS_NR&client_secret=sxZOMlP4bucfkVwRho5yfU1U1pLUyWgv&redirect_uri=https://example.com/oauthDemo&code=CBNCKBAAHBCAABAA4Yu-Ob_h4dfN843wTEUjhPl2SVs9thLX";
-	fetch(
-		url + urlParams,
-		{
-			method: "POST",
-			headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-		}
-	).then(res => res.json()).then(console.log)
+
+	var client_id = localStorage.getItem("client_id");
+	var client_secret = localStorage.getItem("client_secret");
+	var authorization_code = localStorage.getItem("authorization_code");
+
+	if (paramExists(client_id) && paramExists(client_secret) && paramExists(authorization_code)) {
+		var urlParams = "?grant_type=" + grant_type +
+						"&client_id=" + client_id + 
+						"&client_secret=" + client_secret + 
+						"&redirect_uri=" + redirect_uri + 
+						"&code=" + authorization_code;
+
+		fetch(url + urlParams, {method: "POST", mode: "no-cors", headers: {"Content-Type": "application/x-www-form-urlencoded"}})
+			.then(res => res.json())
+			.then(console.log)
+
+	}
+}
+
+function paramExists(param) {
+	return param && param.trim().length;
 }
 
